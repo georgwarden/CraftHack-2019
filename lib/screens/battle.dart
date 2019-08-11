8a0d2bf90cc63f66a8e28c040cd21ce84e179847
Egor <yytruten@mts.ru>
2019-08-11 10:42:47 +0300

    create battle screen

---
+++ lib/screens/battle.dart
@@ -0,0 +1,18 @@
+
+
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+
+class BattleScreen extends StatelessWidget {
+
+  final Stream socketEvents;
+
+  BattleScreen({this.socketEvents});
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
